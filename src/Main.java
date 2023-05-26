414fb5b8e7690ba25bb4bdf955629517b6b678ca
Soliman-crypto <134646718+Soliman-crypto@users.noreply.github.com>
2023-05-26 22:23:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Create some sample products
+      
         Book book1 = new Book(29.99, 400, 5000, 100, "John Doe", "1234567890");
         Journal journal1 = null;
         try {
@@ -15,14 +15,14 @@ class Main {
         }
         Magazine magazine1 = new Magazine(4.99, 50, 3000, 20, "Bob Johnson", "Weekly");
 
-        // Display a menu for the user to choose a product
+       
         System.out.println("Please choose a product to view:");
         System.out.println("1. Book");
         System.out.println("2. Journal");
         System.out.println("3. Magazine");
         int choice = scanner.nextInt();
 
-        // Display the information for the chosen product
+        
         switch (choice) {
             case 1:
                 book1.displayProductInformation();
